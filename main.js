5ad1e86cccc0b6f0a53c35bb1094512f12bd53e7
Funky-Juice <isa86box@gmail.com>
2017-04-30 01:01:49 +0300

    1:01

--- main.js
+++ main.js
@@ -1,5 +1,44 @@
 'use strcit';
 
+var jsonData = [{
+  "users": [
+    {
+      "id": 1,
+      "name": "Иванов Иван",
+      "login": "Ivan1988",
+      "email": "Ivan1988@fake.com",
+      "created": "2017-04-29T09:35:30.666Z"
+    },
+    {
+      "id": 2,
+      "name": "Петров Петр",
+      "login": "PetyaPetushok1986",
+      "email": "PetyaPetushok@fake.com",
+      "created": "2017-04-20T09:35:30.666Z"
+    },
+    {
+      "id": 3,
+      "name": "Содоров Сидор",
+      "login": "Sidor1985",
+      "email": "SidorFedor@fake.com",
+      "created": "2017-03-18T09:35:30.666Z"
+    }
+  ]
+}]
+
+var content = function content(data) {
+  return '<p class="game__task">' + data.task + '</p>\n <form class="game__content">\n\n' +
+
+    data.gameOption.map(function (gameOption) {
+      return '<div class="game__option">\n <img src="' + gameOption.image + '" alt="' + gameOption.alt + '" width="468" height="458">\n\n' +
+
+        gameOption.answerParams.map(function (param) {
+          return '<label class="game__answer  ' + param.class + '">\n <input name="' + param.name + '" type="radio" value="' + param.value + '">\n <span>' + param.text + '</span>\n </label>';
+        }).join('') + '\n\n </div>';
+    }).join('') + '\n  </form>';
+};
+
+
 var templateTable = function(data) {
   return '<table class="table">' +
             '<tr>' +
@@ -10,16 +49,26 @@ var templateTable = function(data) {
               '<th>created</th>' +
             '<tr>' +
             '<tr>' +
-              '<td>' + data.id + '</td>' +
-              '<td>' + data.name + '</td>' +
-              '<td>' + data.login + '</td>' +
-              '<td>' + data.email + '</td>' +
-              '<td>' + data.created + '</td>' +
+              '<td>' + data.users[0].id + '</td>' +
+              '<td>' + data.users[0].name + '</td>' +
+              '<td>' + data.users[0].login + '</td>' +
+              '<td>' + data.users[0].email + '</td>' +
+              '<td>' + data.users[0].created + '</td>' +
+            '<tr>' +
+            '<tr>' +
+              '<tr>' +
+              '<td>' + data.users.map(function(users) {
+      users.id
+    }).id + '</td>' +
+              '<td>' + data.users[1].name + '</td>' +
+              '<td>' + data.users[1].login + '</td>' +
+              '<td>' + data.users[1].email + '</td>' +
+              '<td>' + data.users[1].created + '</td>' +
             '<tr>' +
           '</table>'
 }
 
 var tableContainer = document.getElementById('app');
-
-tableContainer.insertAdjacentHTML('beforeend', templateTable());
+console.log(jsonData[0].users[0].name);
+tableContainer.insertAdjacentHTML('beforeend', templateTable(jsonData[0]));
 
