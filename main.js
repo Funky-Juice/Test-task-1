03ba6cdcaec1b2ee2c277948f3ba8bc05f958f68
Funky-Juice <isa86box@gmail.com>
2017-04-30 02:01:30 +0300

    создал шаблон для отрисовки таблицы

--- main.js
+++ main.js
@@ -26,18 +26,6 @@ var jsonData = [{
   ]
 }]
 
-var content = function content(data) {
-  return '<p class="game__task">' + data.task + '</p>\n <form class="game__content">\n\n' +
-
-    data.gameOption.map(function (gameOption) {
-      return '<div class="game__option">\n <img src="' + gameOption.image + '" alt="' + gameOption.alt + '" width="468" height="458">\n\n' +
-
-        gameOption.answerParams.map(function (param) {
-          return '<label class="game__answer  ' + param.class + '">\n <input name="' + param.name + '" type="radio" value="' + param.value + '">\n <span>' + param.text + '</span>\n </label>';
-        }).join('') + '\n\n </div>';
-    }).join('') + '\n  </form>';
-};
-
 
 var templateTable = function(data) {
   return '<table class="table">' +
@@ -48,23 +36,16 @@ var templateTable = function(data) {
               '<th>e-mail</th>' +
               '<th>created</th>' +
             '</tr>' +
-            '<tr>' +
-              '<td>' + data.users[0].id + '</td>' +
-              '<td>' + data.users[0].name + '</td>' +
-              '<td>' + data.users[0].login + '</td>' +
-              '<td>' + data.users[0].email + '</td>' +
-              '<td>' + data.users[0].created + '</td>' +
-            '</tr>' +
 
-              data.users.map(function(users) {
-                '<tr>' +
-                  '<td>' + users.id + '</td>' +
-                  '<td>' + users.name + '</td>' +
-                  '<td>' + users.login + '</td>' +
-                  '<td>' + users.email + '</td>' +
-                  '<td>' + users.created + '</td>' +
-                '</tr>'
-              }).join('')
+  data.users.map(function(users) {
+    return '<tr>' +
+              '<td>' + users.id + '</td>' +
+              '<td>' + users.name + '</td>' +
+              '<td>' + users.login + '</td>' +
+              '<td>' + users.email + '</td>' +
+              '<td>' + users.created + '</td>' +
+            '</tr>'
+  }).join('')
 
           '</table>'
 }
@@ -72,10 +53,18 @@ var templateTable = function(data) {
 var tableContainer = document.getElementById('app');
 // console.log(jsonData[0].users[0].name);
 
-console.log(jsonData[0].users.map(function(users) {
-    console.log(users.name);
-})
-);
-
 tableContainer.insertAdjacentHTML('beforeend', templateTable(jsonData[0]));
+
+
+
+
+
+
+
+
+
+
+
+
+
 
