a6cd30fe2e8d34a3c5e97347bc4155d30ea065bf
Funky-Juice <isa86box@gmail.com>
2017-04-30 03:12:17 +0300

    3:12 добавил кнопку удаления строки

--- main.js
+++ main.js
@@ -45,15 +45,39 @@ var templateTable = function(data) {
               '<td>' + users.email + '</td>' +
               '<td>' + users.created + '</td>' +
             '</tr>'
+
+            // '<input class="delete-btn" type="button" value="Удалить">'
   }).join('')
 
           '</table>'
 }
 
 var tableContainer = document.getElementById('app');
-// console.log(jsonData[0].users[0].name);
 
 tableContainer.insertAdjacentHTML('beforeend', templateTable(jsonData[0]));
+
+var table = tableContainer.querySelector('.table');
+var tableBody = table.querySelector('tbody');
+var tableRow = table.getElementsByTagName('tr');
+
+
+for (var i = 1; i < tableRow.length; i++) {
+  var btn = document.createElement('input');
+  btn.setAttribute('type', 'button');
+  btn.setAttribute('value', 'Удалить');
+  btn.classList.add('delete-btn');
+
+  tableRow[i].appendChild(btn);
+}
+
+var deleteRawBtn = table.querySelectorAll('.delete-btn');
+
+for (var p = 0; p < deleteRawBtn.length; p++) {
+  deleteRawBtn[p].addEventListener('click', function() {
+    var tr = this.parentNode;
+    tableBody.removeChild(tr);
+  });
+}
 
 
 
