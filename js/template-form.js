5062ba7f92a21abcd22dbd21cc5dab14e54bb32e
Funky-Juice <isa86box@gmail.com>
2017-05-03 02:53:38 +0300

    2:53

--- js/template-form.js
+++ js/template-form.js
@@ -2,7 +2,26 @@
 
 var templateForm = function() {
   return '<form class="user-form">' +
-            '<input class="text-field__input" type="text" name="user_name" placeholder="Укажите имя" required>' +
+            '<label class="user-form__item">' +
+               '<span class="user-form__field-name">Имя: </span>' +
+               '<input class="user-form__input" type="text" name="user_name" placeholder="Укажите имя" required>' +
+            '</label>' +
+
+            '<label class="user-form__item">' +
+              '<span class="user-form__field-name">Логин: </span>' +
+              '<input class="user-form__input" type="text" name="user_name" placeholder="Login" required>' +
+            '</label>' +
+
+            '<label class="user-form__item">' +
+              '<span class="user-form__field-name">Электронная почта: </span>' +
+              '<input class="user-form__input" type="text" name="user_name" placeholder="example@mail.com" required>' +
+            '</label>' +
+
+            '<label class="user-form__item">' +
+              '<span class="user-form__field-name">Дата регистрации: </span>' +
+              '<input class="user-form__input" type="text" name="user_name" placeholder="Registration date" required>' +
+            '</label>' +
+
             '<input class="user-form__send-btn" type="submit">' +
           '</form>'
 };
