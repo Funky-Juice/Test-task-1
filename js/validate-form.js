f0aed6987811625001f8d2a816259fcd41c1b185
Funky-Juice <isa86box@gmail.com>
2017-05-05 02:30:17 +0300

    pause 2:30

--- js/validate-form.js
+++ js/validate-form.js
@@ -1,7 +1,25 @@
 'use strict';
 
-function validateForm() {
+function showError(input) {
 
+}
+
+function validateForm() {
+  var userForm = document.querySelector('#app .user-form');
+  var formInputs = userForm.querySelectorAll('.user-form__input');
+
+  for(var i = 0; i < formInputs.length; i++) {
+
+    var input = formInputs[i];
+
+    var pattern = input.getAttribute('pattern');
+
+    pattern = new RegExp(pattern);
+
+    if (pattern.test(input.value) !== true) {
+      showError(input);
+    }
+  };
 }
 
 module.exports = validateForm;
